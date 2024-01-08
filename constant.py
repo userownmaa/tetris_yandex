499f56247426dc31f8dfa510ee58150cab5fd29c
tony <momina06@mail.ru>
2024-01-08 19:25:48 +0300

    first commit

---
+++ constant.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class Constant:
+    BLACK = pygame.Color("black")
+    WHITE = pygame.Color("white")
+
+    SIZE = WIDTH, HEIGHT = (600, 600)
+    FPS = 60
+    BLOCK = 20
+    FIELD_WIDTH, FIELD_HEIGHT = 10, 20
+    MARGIN_SIDE = (WIDTH - FIELD_WIDTH * BLOCK) / 2
+    MARGIN_TOP = HEIGHT - (FIELD_HEIGHT *BLOCK) - 20
---
+++ field.py
@@ -0,0 +1,6 @@
+from constant import Constant
+
+class Field:
+
+    def __init__(self):
+        self.matrix =
